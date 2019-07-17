ef75e4e106eca48f52344b770342213058f8eb4f
Tiger <atlantisspeed@gmail.com>
2019-07-18 07:25:15 +0800

    container 使用

--- lib/nivagation/SecondRoute.dart
+++ lib/nivagation/SecondRoute.dart
@@ -1,4 +1,7 @@
+import 'dart:ui' as prefix0;
+
 import 'package:flutter/material.dart';
+import 'package:flutter_dev/layout/ContainerLayoutWidgets.dart';
 
 class SecondRoute extends StatelessWidget {
   @override
@@ -7,14 +10,36 @@ class SecondRoute extends StatelessWidget {
       appBar: AppBar(
         title: Text("第二个页面"),
       ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('返回上一个页面'),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            BackButton(),
+            Text('文本1'),
+            Text('文本2'),
+            Text('文本3'),
+            Text('文本1'),
+            Text('文本2'),
+            Text('文本3'),
+            RaisedButton(
+              child: Text('打开下一个页面'),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => ContainerLayoutWidgets()));
+              },
+            ),
+          ],
         ),
       ),
+      // body: Center(
+      //   child: RaisedButton(
+      //     onPressed: () {
+      //       Navigator.pop(context);
+      //     },
+      //     child: Text('返回上一个页面'),
+      //   ),
+      // ),
     );
   }
 }
